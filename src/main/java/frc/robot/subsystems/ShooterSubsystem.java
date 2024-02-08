13639e8898ba25f9ce6729c0374e57c408e259a4
Pat Stoffel <pstoffel3383@gmail.com>
2024-02-08 13:17:26 -0600

    Added Rev JSON

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -7,25 +7,33 @@ package frc.robot.subsystems;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.Follower;
 import com.ctre.phoenix6.hardware.TalonFX;
+import com.revrobotics.CANSparkMax;
 
+import edu.wpi.first.wpilibj.drive.RobotDriveBase.MotorType;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ShooterSubsystem extends SubsystemBase {
+    private CANSparkMax leftShooterSparkMax;
+    private CANSparkMax rightShooterSparkMax;
 
-      private final TalonFX leftShooterFX  = new TalonFX(10, "rio");
+
+ /*     private final TalonFX leftShooterFX  = new TalonFX(10, "rio");
       private final TalonFX rightShooterFX = new TalonFX(11, "rio");
+*/
 
-     
 
   public ShooterSubsystem() {
 
-            /* Reset Talon Configs -- Clears out old values */
-      leftShooterFX.getConfigurator().apply(new TalonFXConfiguration());
-      rightShooterFX.getConfigurator().apply(new TalonFXConfiguration());
+    leftShooterSparkMax = new CANSparkMax(15,MotorType.kBrushless);
 
+
+            /* Reset Talon Configs -- Clears out old values */
+ /*      leftShooterFX.getConfigurator().apply(new TalonFXConfiguration());
+      rightShooterFX.getConfigurator().apply(new TalonFXConfiguration());
+*/
             /* Set followers to follow leader */
-        rightShooterFX.setControl(new Follower(leftShooterFX.getDeviceID(), true));
+ //       rightShooterFX.setControl(new Follower(leftShooterFX.getDeviceID(), true));
   }
 
 
@@ -38,13 +46,17 @@ public class ShooterSubsystem extends SubsystemBase {
   @Override
   public void periodic() {
     
-      SmartDashboard.putNumber("Shooter Velocity", leftShooterFX.getVelocity().getValueAsDouble());  // NOW IN REV/SEC
+ //     SmartDashboard.putNumber("Shooter Velocity", leftShooterFX.getVelocity().getValueAsDouble());  // NOW IN REV/SEC
+
   }
 
 
   public void setShooterSpeeds(double voltage) {
-      leftShooterFX.setVoltage(voltage);
+//      leftShooterFX.setVoltage(voltage);
+
   }
+
+  
 
 
 }
